0619f6f996ba5f6f844863498f171beb573e97a0
emirmartinez <emirmartinez@gmail.com>
2020-05-06 19:33:14 -0700

    initial commit

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx'
+
+console.log("Hello World");
+ReactDOM.render(<App/>,document.getElementById('app'));
+
+
